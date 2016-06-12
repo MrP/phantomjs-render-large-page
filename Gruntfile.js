e4037a709129b24afb852dbeeb18cb11c64be0bd
Victor Rodriguez Gil <mrpatan@gmail.com>
2016-06-12 11:18:52 +0000

    cleanup before publishing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,7 @@
 module.exports = function (grunt) {
-    function pageName() {
-        if (grunt.option('newPage')) {
-            return grunt.option('newPage').replace(/\.html$/i,'');
-        } else if (grunt.option('originalPage')) {
-            return grunt.option('originalPage').replace(/\.html$/i,'') + '-tiled';
-        } else {
-            return 'page';
-        }
-    }
-    function originalPage() {
-        var orig = grunt.option('originalPage');
-        if (!/\.html$/i.test(orig)) {
-            orig += '.html';
-        }
-        return orig;
-    }
-    function filesDir() {
-        return pageName() + '-files';
-    }
-
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
-        pageName: pageName(),
-        filesDir: filesDir(),
-        'http-server': {
-            test: {
-                root: 'spec/test-pages/',
-                port: process.env.PORT || '8080',
-                host: process.env.IP || '0.0.0.0',
-                runInBackground: true
-            },
-            dev: {
-                root: 'spec/test-pages/',
-                port: process.env.PORT || '8080',
-                host: process.env.IP || '0.0.0.0',
-                runInBackground: false
-            }
-        },
         jasmine_nodejs: {
             options: {
                 specNameSuffix: "spec.js",
@@ -53,15 +17,6 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('build', function () {
-        // grunt.task.run(['']);
-    });
-
-    grunt.registerTask('test', function () {
-        // grunt.task.run(['']);
-    });
-
     grunt.registerTask('test', ['jasmine_nodejs']);
-    grunt.registerTask('server', ['http-server:dev']);
-    grunt.registerTask('default', ['tile', 'build', 'server']);
+    grunt.registerTask('default', ['test']);
 };
